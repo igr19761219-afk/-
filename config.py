6ddbcbcadae1be10d19cb18c9773db9a86bd957b
igr19761219-afk <igr19761219@gmail.com>
2026-03-16 12:24:16 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN', '7950270754:AAFlG32tYfskbyS5D_KWbt8_7oQTlmVGgrw')
+ADMIN_CHAT_ID = 138948169
+VIDEO_LINK = "https://disk.yandex.ru/i/vW-iO1qTdH1xbA"
+SILENT_DAYS = 3
